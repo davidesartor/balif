6b805b7b1a2b2d15f7167884570bc2532ea8ec06
Davide Sartor <davide.sartor.4@studenti.unipd.it>
2024-02-15 15:52:37 +0100

    Refactor variable names for clarity and consistency

--- jax/isolation_forest.py
+++ jax/isolation_forest.py
@@ -6,67 +6,53 @@ import jax.numpy as jnp
 import numpy as np
 
 
-@jax.jit
-def expected_depth(node_sizes: jax.Array) -> jax.Array:
-    """Compute the expected isolation depth for a node with n data points."""
-
-    def correction(n: jax.Array) -> jax.Array:
-        EULER_MASCHERONI = 0.5772156649
-        harm_number = jnp.log(n - 1) + EULER_MASCHERONI
-        expected_depth = 2 * harm_number - 2 * (n - 1) / n
-        return expected_depth
-
-    return jnp.where(node_sizes <= 1, 0.0, correction(node_sizes))
-
-
-@partial(jax.jit, static_argnames=("dim", "non_zero_components"))
-def sample_vector(rng: jax.Array, dim: int, non_zero_components: int) -> jax.Array:
-    """Sample a (possibly sparse) vector uniformly on the unit hypersphere."""
-    rng_value, rng_idx = jax.random.split(rng)
-    idxs = jax.random.choice(rng_idx, dim, (non_zero_components,), replace=False)
-    values = jax.random.normal(rng_value, shape=(non_zero_components,))
-    vector = jnp.zeros((dim,)).at[idxs].set(values)
-    return vector / jnp.linalg.norm(vector)
-
-
-@jax.jit
-def sample_intercept(rng: jax.Array, distances: jax.Array, mask: jax.Array):
-    """Sample an intercept that splits the data points."""
-    min_distance = jnp.where(mask, distances, jnp.inf).min()
-    max_distance = jnp.where(mask, distances, -jnp.inf).max()
-    intercept = jax.random.uniform(rng, (), minval=min_distance, maxval=max_distance)
-    return intercept
-
-
 class IsolationTree(struct.PyTreeNode):
     normals: jax.Array  # shape (nodes, features)
     intercepts: jax.Array  # shape (nodes,)
-    depths: jax.Array  # shape (nodes,)
     node_sizes: jax.Array  # shape (nodes,)
-    expected_subtree_depth: jax.Array  # shape (nodes,)
+    # expected_subtree_depth: jax.Array  # shape (nodes,)
+
+    @staticmethod
+    def depth(node_id: jnp.int_) -> jnp.int_:
+        return jnp.log2(1 + node_id).astype(int)
+
+    @staticmethod
+    def expected_depth(node_size: jnp.int_) -> jnp.float_:
+        """Compute the expected isolation depth for a node with n data points."""
+        EULER_MASCHERONI = 0.5772156649
+        harm_number = jax.lax.select(node_size > 1, jnp.log(node_size - 1) + EULER_MASCHERONI, 0.0)
+        expected_depth = 2 * harm_number - 2 * (node_size - 1) / node_size
+        return expected_depth
 
     @jax.jit
     def path(self, point: jax.Array) -> jax.Array:
         """Return the path for a given point in the tree as an Array of node idx."""
+        n_nodes, n_features = self.normals.shape
 
-        def scan_body(id, x):
-            distance = jnp.dot(self.normals[id], point) - self.intercepts[id]
-            child = jax.lax.select(distance >= 0, 2 * id + 1, 2 * id + 2)
-            return child, id
+        def scan_body(node_id, x):
+            distance = jnp.dot(self.normals[node_id], point) - self.intercepts[node_id]
+            child = jax.lax.select(distance >= 0, 2 * node_id + 1, 2 * node_id + 2)
+            return child, node_id
 
-        root = jnp.zeros((), dtype=int)
-        max_depth = int(np.log2(self.node_sizes.size + 1)) - 1
-        _, path = jax.lax.scan(scan_body, root, None, length=max_depth)
+        max_depth = int(np.log2(n_nodes + 1)) - 1
+        _, path = jax.lax.scan(scan_body, 0, None, length=max_depth)
         return path
 
     @jax.jit
-    def score(self, point: jax.Array) -> jax.Array:
-        """Return the isolation score for a given point."""
-        path = self.path(point)
-        isolation_node = path[self.node_sizes[path].argmin()]
-        isolation_depth = self.depths[isolation_node]
-        correction = self.expected_subtree_depth[isolation_node]
-        return 2 ** -((isolation_depth + correction) / self.expected_subtree_depth[0])
+    def isolation_node(self, point: jax.Array) -> jnp.int_:
+        """Return the leaf for a given point in the tree as an Array of node idx."""
+        n_nodes, n_features = self.normals.shape
+
+        def while_cond(node_id):
+            return (node_id < n_nodes) & (self.node_sizes[node_id] > 1)
+
+        def while_body(node_id: jnp.int_) -> jnp.int_:
+            distance = jnp.dot(self.normals[node_id], point) - self.intercepts[node_id]
+            child_id = jax.lax.select(distance >= 0, 2 * node_id + 1, 2 * node_id + 2)
+            return child_id
+
+        isolation_node = jax.lax.while_loop(while_cond, while_body, 0)
+        return isolation_node
 
     @classmethod
     @partial(jax.jit, static_argnames=("cls", "hyperplane_components"))
@@ -78,16 +64,21 @@ class IsolationTree(struct.PyTreeNode):
         rng_normal, rng_intercept = jax.random.split(rng)
 
         # for each node, initialize intercepts to 0.0 and presample normal vector
+        def sample_normal(rng: jax.Array) -> jax.Array:
+            rng_value, rng_idx = jax.random.split(rng)
+            idxs = jax.random.choice(rng_idx, features, (hyperplane_components,), replace=False)
+            values = jax.random.normal(rng_value, shape=(hyperplane_components,))
+            vector = jnp.zeros((features,)).at[idxs].set(values)
+            return vector / jnp.linalg.norm(vector)
+
         intercepts = jnp.zeros((nodes,))
-        sample_normal_batch = jax.vmap(sample_vector, in_axes=(0, None, None))
-        normals = sample_normal_batch(
-            jax.random.split(rng_normal, nodes), features, hyperplane_components
-        )
+        normals = jax.vmap(sample_normal)(jax.random.split(rng_normal, nodes))
+
         # precompute dot products between data points and normals
         # doing it this way technically requires more compute but
         # it is faster in practice, especially on GPU due better memory patterns
         dot_prod_batch = jax.vmap(jax.vmap(jnp.dot, in_axes=(0, None)), in_axes=(None, 0))
-        distances = dot_prod_batch(data, normals)
+        distances = dot_prod_batch(data, normals)  # shape (nodes, points)
 
         # keep track of which nodes have been reached by each data point
         # splitting with a single point "forks" the path, so this needs to be
@@ -106,6 +97,12 @@ class IsolationTree(struct.PyTreeNode):
             layer_slice = jnp.arange(2**depth - 1, 2**depth - 1 + 2**depth)
 
             # update the intercepts to make the plane split the data points
+            def sample_intercept(rng: jax.Array, distances: jax.Array, mask: jax.Array):
+                min_distance = jnp.where(mask, distances, jnp.inf).min()
+                max_distance = jnp.where(mask, distances, -jnp.inf).max()
+                intercept = jax.random.uniform(rng, (), minval=min_distance, maxval=max_distance)
+                return intercept
+
             intercepts_at_slice = jax.vmap(sample_intercept)(
                 jax.random.split(rng_layer, 2**depth),
                 distances[layer_slice],
@@ -120,9 +117,9 @@ class IsolationTree(struct.PyTreeNode):
             reached = reached.at[2 * layer_slice + 2, :].set(
                 reached[layer_slice] & (distances[layer_slice] <= intercepts[layer_slice, None])
             )
+
         node_sizes = reached.sum(axis=1)
-        depths = jnp.log2(1 + jnp.arange(nodes)).astype(int)
-        return cls(normals, intercepts, depths, node_sizes, expected_depth(node_sizes))
+        return cls(normals, intercepts, node_sizes)
 
 
 class ExtendedIsolationForest(struct.PyTreeNode):
@@ -130,34 +127,40 @@ class ExtendedIsolationForest(struct.PyTreeNode):
 
     @jax.jit
     def score(self, point: jax.Array) -> jax.Array:
-        tree_scores = jax.vmap(IsolationTree.score, in_axes=(0, None))(self.trees, point)
-        return jnp.exp(jnp.log(tree_scores).mean())
+        def normalized_depth(tree, point) -> jnp.float_:
+            isolation_node = tree.isolation_node(point)
+            isolation_depth = tree.depth(isolation_node)
+            correction = tree.expected_depth(tree.node_sizes[isolation_node])
+            return (isolation_depth + correction) / tree.expected_depth(tree.node_sizes[0])
+
+        normalized_depths = jax.vmap(normalized_depth, in_axes=(0, None))(self.trees, point)
+        return 2 ** -normalized_depths.mean()
 
     @jax.jit
     def score_samples(self, data: jax.Array) -> jax.Array:
         return jax.vmap(self.score)(data)
 
     @classmethod
-    @partial(jax.jit, static_argnums=(0, 3, 4, 5, 6))
+    @partial(
+        jax.jit, static_argnames=("cls", "n_estimators", "max_samples", "hyperplane_components")
+    )
     def fit(
         cls,
         rng: jax.Array,
         data: jax.Array,
-        n_estimators: int,
+        *,
+        n_estimators: int = 128,
         max_samples: Optional[int] = None,
         hyperplane_components: Optional[int] = None,
         bootstrap: bool = True,
     ):
-        rng_subsample, rng_forest = jax.random.split(rng)
         max_samples = max_samples or min((256, data.shape[0]))
+        hyperplane_components = hyperplane_components or data.shape[-1]
 
-        subsamples = jax.vmap(
-            partial(jax.random.choice, a=data, shape=(max_samples,), replace=bootstrap)
-        )(jax.random.split(rng_subsample, n_estimators))
+        def fit_tree(rng):
+            rng_sample, rng_fit = jax.random.split(rng)
+            subsample = jax.random.choice(rng_sample, data, (max_samples,), replace=bootstrap)
+            return IsolationTree.fit(rng_fit, subsample, hyperplane_components)
 
-        trees = jax.vmap(IsolationTree.fit, in_axes=(0, 0, None))(
-            jax.random.split(rng_forest, n_estimators),
-            subsamples,
-            hyperplane_components or data.shape[-1],
-        )
+        trees = jax.vmap(fit_tree)(jax.random.split(rng, n_estimators))
         return cls(trees)
